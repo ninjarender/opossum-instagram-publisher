eb0c88e631773285428fb0f51c76d49c96b45704
Vadym Kruchyna <kruchina.vadim@gmail.com>
2025-08-01 19:37:57 +0200

    init

---
+++ lib/opossum.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "opossum/version"
+
+module Opossum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/opossum_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Opossum do
+  it "has a version number" do
+    expect(Opossum::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
